f6a6d889c1cd26edc564c68a08c966bcede2b40c
Silas <silas@irmisch.de>
2022-12-23 13:36:42 +0100

    maker fix

--- forge.config.js
+++ forge.config.js
@@ -3,13 +3,6 @@ module.exports = {
 	rebuildConfig: {},
 	makers: [
 		{
-			name: '@electron-forge/maker-squirrel',
-			config: {
-				certificateFile: './cert.pfx',
-				certificatePassword: process.env.CERTIFICATE_PASSWORD
-			}
-		},
-		{
 			name: '@electron-forge/maker-zip'
 		}
 	]
